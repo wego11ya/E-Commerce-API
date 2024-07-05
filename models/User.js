b27732d30fcd15a705ae25d3a7ff4380bd4a967b
wego11ya <wego11ya@gmail.com>
2024-07-05 09:39:32 +0800

    feat: add validator

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,6 +10,10 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide an email"],
+    validate: {
+      validator: validator.isEmail,
+      messsage: "Please provide a valid email",
+    },
   },
   password: {
     type: String,
