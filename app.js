5a6607a343e55060502d152f301c0790f6a5714a
wego11ya <wego11ya@gmail.com>
2024-07-10 09:48:28 +0800

    feat: auth routes setup

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const morgan = require("morgan");
 
 // database connection
 const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/authRoutes");
+
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -19,6 +23,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("e-commerce api");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
