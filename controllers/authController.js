056bd6051045f2fae15da59ace3fab45f11177aa
wego11ya <wego11ya@gmail.com>
2024-07-10 17:04:14 +0800

    feat: JWT setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const { createJWT } = require("../utils");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,7 +16,15 @@ const register = async (req, res) => {
   const role = isFirstAccount ? "admin" : "user";
 
   const user = await User.create({ email, name, password, role });
-  res.status(StatusCodes.CREATED).json({ user });
+  const tokenUser = {
+    name: user.name,
+    userId: user._id,
+    email: user.email,
+    role: user.role,
+  };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send("Login");
