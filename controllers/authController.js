046cae0542ab42a489d7e2513cfe74e4766d2a71
wego11ya <wego11ya@gmail.com>
2024-07-10 10:54:34 +0800

    feat: add authController.register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,17 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 const register = async (req, res) => {
-  res.send("Register");
+  const { email } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send("Login");
