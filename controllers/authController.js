522a67652a7f12cd14cec2f6fc6ddfd255561f0e
wego11ya <wego11ya@gmail.com>
2024-07-11 15:46:03 +0800

    feat: add authController.login

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,30 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
-  res.send("Login");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  const tokenUser = {
+    name: user.name,
+    userId: user._id,
+    email: user.email,
+    role: user.role,
+  };
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
   res.send("Logout");
