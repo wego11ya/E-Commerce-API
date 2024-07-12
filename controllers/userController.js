adb8d8df5327fad39eadd60bdd43433b357ea7fc
wego11ya <wego11ya@gmail.com>
2024-07-12 15:22:49 +0800

    feat: add userController.updateUserPassword

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,19 @@ const updateUser = async (req, res) => {
   res.send("update user");
 };
 const updateUserPassword = async (req, res) => {
-  res.send("update user password");
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError("Please provide both values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  user.password = newPassword;
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Password updated!" });
 };
 
 module.exports = {
