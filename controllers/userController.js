07cce83e603536ec4e34325ceafddb141cfd179b
wego11ya <wego11ya@gmail.com>
2024-07-12 09:51:56 +0800

    feat: add userController.getAllUsers and getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,17 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 const getAllUsers = async (req, res) => {
-  res.send("get all users");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
 const getSingleUser = async (req, res) => {
-  res.send("get single user");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 const showCurentUser = async (req, res) => {
   res.send("show current user");
