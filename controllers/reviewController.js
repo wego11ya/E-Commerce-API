8db95b7e7ddf0006151cc5975434517637b5addc
wego11ya <wego11ya@gmail.com>
2024-07-15 14:28:48 +0800

    feat: add populate to reviewController.getAllReviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -25,7 +25,10 @@ const createReview = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({});
+  const reviews = await Review.find({}).populate({
+    path: "productId",
+    select: "name company price",
+  });
   res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
 const getSingleReview = async (req, res) => {
