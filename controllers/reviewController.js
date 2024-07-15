77081808756abbf16b2e1f9021aba407f76319d1
wego11ya <wego11ya@gmail.com>
2024-07-15 10:39:34 +0800

    feat: add reviewController.createReview

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,14 +3,26 @@ const Product = require("../models/Product");
 
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const {
-  createTokenUser,
-  attachCookiesToResponse,
-  checkPermissions,
-} = require("../utils");
+const { checkPermissions } = require("../utils");
 
 const createReview = async (req, res) => {
-  res.send("Create Review");
+  const { productId } = req.body;
+  const isValidProduct = await Product.findById(productId);
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  }
+  const alreadyReviewed = await Review.findOne({
+    productId,
+    userId: req.user.userId,
+  });
+  if (alreadyReviewed) {
+    throw new CustomError.BadRequestError(
+      "You have already reviewed this product"
+    );
+  }
+  req.body.userId = req.user.userId;
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
   res.send("Get All Reviews");
