309333779ad7a2c61410897a0a880eafa2547e76
wego11ya <wego11ya@gmail.com>
2024-07-17 16:46:38 +0800

    feat: use MongoDB aggregation pipeline to claculate average rating and number of reviews

--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,42 @@ const ReviewSchema = new mongoose.Schema(
 // only one review per user per product
 ReviewSchema.index({ userId: 1, productId: 1 }, { unique: true });
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    {
+      $match: { productId: productId },
+    },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Product").findByIdAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.productId);
+});
+ReviewSchema.post(
+  "deleteOne",
+  { document: true, query: false },
+  async function () {
+    await this.constructor.calculateAverageRating(this.productId);
+  }
+);
+
 module.exports = mongoose.model("Review", ReviewSchema);
