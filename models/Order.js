833a53e5ed44b06fcb4c6a4194eed4b53cd2e364
wego11ya <wego11ya@gmail.com>
2024-07-18 13:04:39 +0800

    feat: add Order Model

---
+++ models/Order.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const SingleCartItemSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  productId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [SingleCartItemSchema],
+    status: {
+      type: String,
+      enum: ["pending", "failed", "paid", "delivered", "cancelled"],
+      default: "pending",
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentItentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
