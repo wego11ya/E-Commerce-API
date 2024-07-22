c19e2c43c35c5b43bd9f26128c5b74a8197125d4
wego11ya <wego11ya@gmail.com>
2024-07-22 22:20:26 +0800

    fix: some small fixes

--- app.js
+++ app.js
@@ -40,21 +40,11 @@ app.use(xss());
 app.use(mongoSanitize());
 app.use(cors());
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("./public"));
 app.use(fileUpload());
-
-app.get("/", (req, res) => {
-  res.send("e-commerce api");
-});
-
-app.get("/api/v1", (req, res) => {
-  console.log(req.signedCookies);
-  res.send("e-commerce api");
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
